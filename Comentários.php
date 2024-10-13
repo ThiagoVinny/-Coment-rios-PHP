a83c5615431f25c51d3e553d89e7af3aa27c1e14
Thiago_Vinny <thiagovinnybrasil@hotmail.com>
2024-10-13 19:48:12 -0300

    first commit

---
+++ Comentários.php
@@ -0,0 +1,14 @@
+<?php
+
+// está variavel contem o nome de uma pessoa 
+$nome = 'Matheus';
+
+// está variável contém a idade de uma pessoa
+$idade = '21 anos';
+
+//está variavel contem a profissão da pessoa
+$profissão = 'Programador';
+
+echo  $nome;
+
+
